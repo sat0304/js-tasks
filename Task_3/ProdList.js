1f5ed21aa0fa2cc520a99a5d88a043002fd957cd
Сергей Токарeв <sat0304@yandex.ru>
2023-03-02 23:46:24 +0400

    product array is moved to other repo

--- Task_3/ProdList.js
+++ Task_3/ProdList.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const MATCHSTR = 'name-contains-fd&price-=2-&quantity->5&description-ends-abc';
-const MATCHSTR2 = 'name-starts-fd&quantity=5';
+const MATCHSTR2 = 'name-starts-fd&quantity-=5';
 let products = new Array();
 
 class ProdList {
@@ -31,12 +31,10 @@ class ProdList {
   
   set name(valueName){
     this._name = valueName;
-
   }
   
   set price(valuePrice){
     this._price = valuePrice;
-    // console.log( valuePrice,  this._price);
   }
     
   set quantity(valueQuantity) {
@@ -47,19 +45,61 @@ class ProdList {
       this._description = valueDescription;
   }
 
+  prodSelect(prodQuery){
+    let prodFeatures = prodQuery.split('&');
+
+    let nameParam = prodFeatures[0].split('-');
+    let priceParam = prodFeatures[1].split('-');
+    let quantityParam = '';
+    let descriptionParam = '';
+
+    if (prodFeatures[2] != undefined) {
+      quantityParam = prodFeatures[2].split('-');
+      descriptionParam = prodFeatures[3].split('-');
+    } else {
+      quantityParam = priceParam;
+    }
+    if ((nameParam[1] == 'contains') || (nameParam[1] == 'starts'))  {
+      if ((this.name.includes(nameParam[2])) &&
+          (this.price == (+priceParam[1][1])) &&
+          (this.quantity >(+quantityParam[1][1])) &&
+          (this.description.endsWith(descriptionParam[2]))) {
+            alert(this.name);
+      } else if ((this.name.startsWith(nameParam[2])) &&
+          (this.quantity >(+quantityParam[1][1]))) {
+            alert(this.name);
+      } else {
+          this.name = 'noMatch';
+          }
+    }
+  }
 }
 
+let productMarker = new ProdList( 'markerfd', 2, 50, 'red clored abc' );
+let productBook = new ProdList( 'lfd_moon', 258, 11, 'amazing story of abc' );
 let productPhone = new ProdList( 'iphone', 1000, 500, 'iphone 15SE model' );
-// console.log( productPhone.name );
-// console.log( productPhone.price );
-// console.log(productPhone.quantity);
-// console.log( productPhone.description );
 let productPencil = new ProdList( 'pencil', 10, 5000, 'black color pencil' );
-let productBook = new ProdList( 'fdmon', 258, 11, 'amazing story of abc' );
 let productDisk = new ProdList( 'disfdra', 2, 5, 'movie of abc' );
-let productMarker = new ProdList( 'fd_marker', 2, 50, 'red clored abc' );
-products = [productPhone, productPencil, productBook, productDisk, productMarker];
-for (let prod of products) {
-  console.log(prod.name)
+
+products = [productMarker, productBook, productPhone, productPencil, productDisk];
+for (let i = 0; i < products.length; i++) {
+  products[i].prodSelect( MATCHSTR );
+  if (products[i].name == 'noMatch') {
+        products.splice(i, 1);
+      };
 }
 
+for (let i = 0; i < products.length; i++) {
+  alert(products[i].name);
+  };
+
+  for (let i = 0; i < products.length; i++) {
+    products[i].prodSelect( MATCHSTR2 );
+    if (products[i].name == 'noMatch') {
+          products.splice(i, 1);
+        };
+  }
+  
+  for (let i = 0; i < products.length; i++) {
+    alert(products[i].name);
+    };
