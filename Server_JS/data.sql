ea6fc8d7d173283ffee429607f36a9e6c82a0ecf
Сергей Токарeв <sat0304@yandex.ru>
2023-03-18 14:13:06 +0400

    db user is changed to  postgres

--- Server_JS/data.sql
+++ Server_JS/data.sql
@@ -1,8 +1,4 @@
 CREATE DATABASE movie_db;
--- CREATE USER movie_user WITH ENCRYPTED PASSWORD 'Spges3spges';
-
--- GRANT ALL PRIVILEGES ON DATABASE movie_db TO movie_user;
--- SET role movie_user;
 connect movie_db;
 
  CREATE TABLE person (
--- Server_JS/server.js
+++ Server_JS/server.js
@@ -1,19 +1,3 @@
-// const http = require('http');
-const os = require('os');
-
-// http.createServer((req, res) => {
-//     console.log(req.url);
-//     console.log(req.headers);
-//     console.log(req.method);
-//     res.writeHead(200,{'Content-Type': 'text/plain'});
-//     res.end('I, programer.');
-// }).listen(3000, () => console.log('server works fine!'));
-
-// console.log(os.platform());
-// console.log(os.arch());
-// console.log(os.cpus());
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = 3000;
